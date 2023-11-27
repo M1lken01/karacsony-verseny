eeb5ee82ecc14b7441acda8eba8a971510c4ba08
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2023-11-28 00:33:45 +0100

    footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,9 +1,41 @@
 <template>
-  <footer class="">&copy; 2023</footer>
+  <footer class="text-center py-20 bg-gray-800 text-white">
+    <div class="p-2">
+      <a href="https://github.com/M1lken01/karacsony-verseny" class="flex items-center justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-2.5 -2.5 21 21" stroke="currentColor" class="h-7 w-7 mr-1">
+          <path
+            d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+          ></path>
+        </svg>
+        Source Code
+      </a>
+    </div>
+    <div class="mt-6 flex flex-col">
+      <span> Made with ❤️ by </span>
+      <span v-for="developer in developers" :key="developer.id" class="p-2" @mouseover="toggleHover(developer)" @mouseout="toggleHover(developer)">
+        {{ developer.hover ? developer.nickname : developer.fullName }}
+      </span>
+    </div>
+    <div class="p-2 mt-6">&copy; 2023 <a href="https://github.com/M1lken01/karacsony-verseny">some Bolyai devs</a>. All rights reserved.</div>
+  </footer>
 </template>
 
 <script>
 export default {
   name: 'FooterSection',
+  data() {
+    return {
+      developers: [
+        { id: 1, nickname: 'm1lk3n01', fullName: 'Artner Milan', hover: false },
+        { id: 2, nickname: 'McBond', fullName: 'Csendes Tamas', hover: false },
+        { id: 3, nickname: 'p_gggg', fullName: 'Pingtizer David', hover: false },
+      ],
+    };
+  },
+  methods: {
+    toggleHover(developer) {
+      developer.hover = !developer.hover;
+    },
+  },
 };
 </script>
