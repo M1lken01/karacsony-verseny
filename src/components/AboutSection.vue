a1bce64a5563cf76c8d0d07dd2db80d0fc5a205f
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2023-11-30 22:19:48 +0100

    about patches

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -2,34 +2,37 @@
   <section id="about" class="flex flex-col text-bg200 bg-fg500">
     <h2 class="mx-8 mt-8 -mb-8 text-left text-2xl">About</h2>
     <div class="flex flex-1 px-10 md:px-32 justify-center md:justify-around items-center gap-8 flex-col md:gap-32 md:flex-row">
-      <div class="md:flex-1 min-w-full md:min-w-0">
-        <h3 class="py-2 text-xl font-bold">Mikor és hol vannak a szakkörök?</h3>
-        <ul>
-          <li>A szakköri foglalkozások a 39-es teremben vannak, szerdánként 14:30-16:00-ig.</li>
-        </ul>
-      </div>
-      <div class="md:flex-1 min-w-full md:min-w-0">
-        <h3 class="py-2 text-xl font-bold">Hogyan lehet bekerülni a tehetségműhelybe?</h3>
-        <ul>
-          <li>Bárki jelentkezhet, akit érdekel.</li>
-          <li>Megnézzük mennyire van érzéke ezekhez a dolgokhoz.</li>
-          <li>Azt fontos tudni, hogy a nevének megfelelően elsődlegesen tehetséggondozással foglalkozunk.</li>
-        </ul>
-      </div>
-      <div class="md:flex-1 min-w-full md:min-w-0">
-        <h3 class="py-2 text-xl font-bold">Hogyan lehet jelentkezni?</h3>
-        <ul>
-          <li>Jelentkezni a Facebook oldalunkon.</li>
-          <li>Személyesen Vinnai Zsolt tanár úrnál.</li>
-          <li>Illetve Kullai Viktória tanárnőnél, az iskola tanárijában.</li>
-        </ul>
-      </div>
+      <AboutSectionItem
+        title="Mikor és hol vannak a szakkörök?"
+        :bulletPoints="['A szakköri foglalkozások a 39-es teremben vannak, szerdánként 14:30-16:00-ig.']"
+      />
+      <AboutSectionItem
+        title="Hogyan lehet bekerülni a tehetségműhelybe?"
+        :bulletPoints="[
+          'Bárki jelentkezhet, akit érdekel.',
+          'Megnézzük mennyire van érzéke ezekhez a dolgokhoz.',
+          'Azt fontos tudni, hogy a nevének megfelelően elsődlegesen tehetséggondozással foglalkozunk.',
+        ]"
+      />
+      <AboutSectionItem
+        title="Hogyan lehet jelentkezni?"
+        :bulletPoints="[
+          'Jelentkezni a Facebook oldalunkon.',
+          'Személyesen Vinnai Zsolt tanár úrnál.',
+          'Illetve Kullai Viktória tanárnőnél, az iskola tanárijában.',
+        ]"
+      />
     </div>
   </section>
 </template>
 
 <script>
+import AboutSectionItem from '@/components/AboutSectionItem.vue';
+
 export default {
   name: 'AboutSection',
+  components: {
+    AboutSectionItem,
+  },
 };
 </script>
---
+++ src/components/AboutSectionItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="md:flex-1 min-w-full md:min-w-0">
+    <h3 class="py-2 text-xl font-bold">{{ title }}</h3>
+    <ul>
+      <li v-for="point in bulletPoints" :key="point">{{ point }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    bulletPoints: Array,
+  },
+};
+</script>
