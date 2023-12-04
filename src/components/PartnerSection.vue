fdb4ed212801cffe45d852735b732d385d6e42bc
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2023-12-04 11:37:45 +0100

    Update PartnerSection.vue

--- src/components/PartnerSection.vue
+++ src/components/PartnerSection.vue
@@ -1,7 +1,6 @@
 <template>
   <section id="partners" class="flex flex-col text-bg200 bg-fg500">
     <h2 class="section-title">Partners</h2>
-    <p>A tehetségműhely munkáját az utóbbi években támogatta a Mobilis és az SMR is.</p>
     <div class="flex-1 grid grid-cols-2 md:grid-cols-3 mx-2 md:mx-16">
       <PartnerItem :imgSrc="require('@//assets/brand/fll-logo-horizontal.png')" alt="FLL Logo" link="https://example.com" />
       <PartnerItem :imgSrc="require('@//assets/brand/fll-logo-horizontal.png')" alt="FLL Logo" link="https://example.com" />
