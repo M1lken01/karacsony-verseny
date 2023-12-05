445d077894c84ce5062dc7d48b8497e8f6398871
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2023-12-05 18:13:12 +0100

    added partners
    -removed unused assets

--- src/components/PartnerItem.vue
+++ src/components/PartnerItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center md:m-16">
     <a :href="link">
-      <img :src="imgSrc" :alt="alt" />
+      <img :src="imgSrc" :alt="alt" class="max-h-32 md:max-h-48 object-contain" />
     </a>
   </div>
 </template>
--- src/components/PartnerSection.vue
+++ src/components/PartnerSection.vue
@@ -2,12 +2,7 @@
   <section id="partners" class="flex flex-col text-bg200 bg-fg500">
     <h2 class="section-title">Partners</h2>
     <div class="flex-1 grid grid-cols-2 md:grid-cols-3 mx-2 md:mx-16">
-      <PartnerItem :imgSrc="require('@//assets/brand/fll-logo-horizontal.png')" alt="FLL Logo" link="https://example.com" />
-      <PartnerItem :imgSrc="require('@//assets/brand/fll-logo-horizontal.png')" alt="FLL Logo" link="https://example.com" />
-      <PartnerItem :imgSrc="require('@//assets/brand/fll-logo-horizontal.png')" alt="FLL Logo" link="https://example.com" />
-      <PartnerItem :imgSrc="require('@//assets/brand/fll-logo-horizontal.png')" alt="FLL Logo" link="https://example.com" />
-      <PartnerItem :imgSrc="require('@//assets/brand/fll-logo-horizontal.png')" alt="FLL Logo" link="https://example.com" />
-      <PartnerItem :imgSrc="require('@//assets/brand/fll-logo-horizontal.png')" alt="FLL Logo" link="https://example.com" />
+      <PartnerItem v-for="partner in partners" :key="partner.id" :imgSrc="partner.imgSrc" :alt="partner.alt" :link="partner.link" />
     </div>
   </section>
 </template>
@@ -19,6 +14,18 @@ export default {
   name: 'PartnerSection',
   components: {
     PartnerItem,
+  },
+  data() {
+    return {
+      partners: [
+        { id: 1, imgSrc: require('@/assets/brand/logo-bolyai.png'), alt: 'Bolyai Logo', link: 'https://bolyaitechnikum.hu/' },
+        { id: 2, imgSrc: require('@/assets/brand/logo-blockbreakers.png'), alt: 'Blockbreakers Logo', link: 'https://www.facebook.com/BlockBreakersBolyai/' },
+        { id: 3, imgSrc: require('@/assets/brand/fll-logo-horizontal.png'), alt: 'FLL Logo', link: 'https://www.firstlegoleague.hu/' },
+        { id: 4, imgSrc: require('@/assets/brand/mobilis.png'), alt: 'Mobilis Logo', link: 'https://mobilis-gyor.hu/' },
+        { id: 5, imgSrc: require('@/assets/brand/smr.png'), alt: 'SMR Logo', link: 'https://www.smr-automotive.com/en/global/mosonmagyarovar/' },
+        { id: 6, imgSrc: require('@/assets/brand/motherson.png'), alt: 'Motherson Logo', link: 'https://www.motherson.com/' },
+      ],
+    };
   },
 };
 </script>
