5ff820c798fa011c05ce220f28ffc8e558ca08a8
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2023-12-05 19:19:43 +0100

    added qr code

--- src/components/PreviewSection.vue
+++ src/components/PreviewSection.vue
@@ -20,16 +20,25 @@
         </iframe>
       </div>
       <div class="flex flex-1 flex-col gap-10">
-        <h3 class="text-xl font-bold -mb-8">Építjük a Jövő Robotikus Tehetségeit!</h3>
+        <h3 class="preview-title">Építjük a Jövő Robotikus Tehetségeit!</h3>
         <p>
           Üdvözöljük a tehetségműhelyben, ahol fiatal tehetségeink robotikával és technológiával bővítik ismereteiket és szárnyalnak a versenyeken. Fedezze fel,
           hogyan alakult ki és fejlődött az elmúlt 7 év során, és ismerje meg a csapatunkat, akik már sikeresen képviselték iskolánkat különböző versenyeken!
         </p>
-        <h3 class="text-xl font-bold -mb-8">Milyen eszközökkel dolgoznak a diákok?</h3>
+        <h3 class="preview-title">Milyen eszközökkel dolgoznak a diákok?</h3>
         <p>
           A tehetségműhelynek 7 darab Lego-robot készlete van. Ezek Ev3-as modellek. Nem a legújabbak, még 7 éve kaptuk őket pályázatok, illetve a Mobilis-SMR
           mentorprogram keretében. Most már lassan időszerű lesz a cseréjük. Egy-egy robot körülbelül 3 évet bír, ilyen mértékű igénybevétel mellett. A mi
           eszközeink ennek már a dupláját teljesítették.
+        </p>
+        <h3 class="preview-title">Amivel még foglalkozunk</h3>
+        <p>
+          A robotikával kapcsolatos elfoglaltságok kívül a tehetségműhely tagjai egyéb informatikai tevékenységekben is részt vehetnek, mint például
+          prezentáció, vagy alkalmazás készítés, melyeknek célja mindig innovációs jellegűek. Az alábbi QR kód az egyik ilyen diákok által fejlesztett
+          alkalmazásra vezet, aminek célja hogy segítsen az embereknek az energiatakarékoskodásban.
+          <a href="https://drive.google.com/file/d/13h9SZMTXyJQ6mPFGXrJ1Ij3cbevUJmey/view?usp=drivesdk">
+            <img src="@/assets/brand/qr-code.png" alt="QR Code" class="w-48 mt-4" />
+          </a>
         </p>
       </div>
     </div>
